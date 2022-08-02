32b441867a08db260d81f0e390e757803024f16d
Felipe <fellipefreiire3@gmail.com>
2022-08-02 18:35:04 -0300

    testing without projects

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,7 +20,7 @@ export const Home = () => {
       </S.HomeContainer>
       <AboutSection />
       <StudySection />
-      <ProjectsSection />
+      {/* <ProjectsSection /> */}
       <BlogSection />
       <Contact />
     </>
